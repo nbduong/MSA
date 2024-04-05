cffad80ff8122ff80b7b6c7814cfdd0890351d64
nhchieu <nhchieu271@gmail.com>
2024-04-06 00:16:11 +0700

    add file

--- app/src/main/java/com/example/msa/MainActivity.java
+++ app/src/main/java/com/example/msa/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView videoList;
+    VideoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        videoList=findViewById(R.id.videoList);
+        videoList.setLayoutManager(new LinearLayoutManager(this));
+        adapter=new VideoAdapter();
+        videoList.setAdapter(adapter);
     }
 }
